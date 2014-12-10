1da11e49c72092b15a8de18e6431b943b442ee87
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2014-12-10 22:54:02 +0530

    Implement `Step` for `int`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::num::Int;
+
 pub struct Range<T> {
     from: T,
     to: T,
@@ -8,4 +10,10 @@ pub trait Step {
     fn zero() -> Self;
     fn one() -> Self;
     fn add(Self, Self) -> Option<Self>;
+}
+
+impl Step for int {
+    fn zero() -> int { 0 }
+    fn one() -> int { 1 }
+    fn add(a: int, b: int) -> Option<int> { a.checked_add(b) }
 }
