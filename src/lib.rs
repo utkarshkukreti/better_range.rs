d60b01e2ca1bb8e26fe0e63feddcef1578906b92
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2014-12-10 23:27:27 +0530

    Add `infinity` fn to `Step` trait.

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,12 @@ pub trait Step {
     fn zero() -> Self;
     fn one() -> Self;
     fn add(Self, Self) -> Option<Self>;
+    fn infinity() -> Self;
 }
 
 impl Step for int {
     fn zero() -> int { 0 }
     fn one() -> int { 1 }
     fn add(a: int, b: int) -> Option<int> { a.checked_add(b) }
+    fn infinity() -> int { Int::max_value() }
 }
