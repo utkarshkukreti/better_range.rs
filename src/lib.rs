c5e9d3c346bde3d61a6f20e236c677b2241914cb
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2014-12-10 23:44:21 +0530

    Rename `Step::add` to `Step::next`.

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,15 @@ pub struct Range<T> {
 pub trait Step {
     fn zero() -> Self;
     fn one() -> Self;
-    fn add(Self, Self) -> Option<Self>;
+    fn next(from: Self, step: Self) -> Option<Self>;
     fn infinity() -> Self;
 }
 
 impl Step for int {
     fn zero() -> int { 0 }
     fn one() -> int { 1 }
-    fn add(a: int, b: int) -> Option<int> { a.checked_add(b) }
+    fn next(from: int, step: int) -> Option<int> {
+        from.checked_add(step)
+    }
     fn infinity() -> int { Int::max_value() }
 }
